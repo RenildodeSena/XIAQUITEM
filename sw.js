2dce83d62dbe585a380dfe82362d1470c460eb67
Renildo de Sena <renildodesena@outlook.com>
2025-05-09 19:44:10 -0300

    Commit inicial do projeto AQUI TEM

---
+++ sw.js
@@ -0,0 +1,22 @@
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('app').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/reportar.html',
+        '/mapa.html',
+        '/relatorios.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
